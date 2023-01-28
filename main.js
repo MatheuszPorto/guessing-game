9edc4dac4245946042c81ce32f274806fca78558
Matheus Mendonça <mattmen16@gmail.com>
2023-01-28 06:50:28 -0300

    feat: added new features

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
 const randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-function handleClick (event) {
-    event.preventDefault()
+// Função CallBack
+function handleTryClick (event) {
+    event.preventDefault() // Remova o padrão
     
     const inputNumber = document.querySelector("#inputNumber")
 
     if (Number(inputNumber.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
-        document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
+        screen1.classList.add("hide")
+        screen2.classList.remove("hide")
+
+        screen2
+            .querySelector(".screen2 h2")
+            .innerText = `You got it in ${xAttempts} tries!`
     }
+
+    inputNumber.value = ''
 
     xAttempts++
 }
+
+// Events
+
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+btnTry.addEventListener('click' , handleTryClick )
+
+btnReset.addEventListener('click' , function () {
+   screen2.classList.add('hide')
+   screen1.classList.remove('hide')
+   xAttempts = 1
+} )
